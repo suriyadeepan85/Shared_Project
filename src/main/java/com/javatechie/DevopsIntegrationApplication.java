97372af1ad0e76212398c58da114bdd42bd79062
suriyadeepan85 <44204574+suriyadeepan85@users.noreply.github.com>
2024-06-15 19:02:54 -0400

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "Welcome to our DevOps Project !!";
+		return "Welcome to our DevOps Project !! Hi From Suriyadeepan";
 	}
 
 	public static void main(String[] args) {
